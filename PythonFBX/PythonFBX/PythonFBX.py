a2afc29905d206b3aa9b6b4298e2652aecf9d8c4
BaggioPereira <baggio_111111@hotmail.com>
2016-01-20 15:33:47 +0000

    Minor changes

--- PythonFBX/PythonFBX/PythonFBX.py
+++ PythonFBX/PythonFBX/PythonFBX.py
@@ -26,7 +26,7 @@ for file in glob.glob("*.fbx"):
     filenames.append(file)
 
 filenum = len(filenames)
-doc = et.Element('svg', width='4800', height='4800', version='1.1', xmlns='http://www.w3.org/2000/svg')
+doc = et.Element('svg', width='480', height='480', version='1.1', xmlns='http://www.w3.org/2000/svg')
 for file in range(filenum):
     if not FbxCommon.LoadScene(sdk_manager, scene, filenames[file]):
         print("Not found")
@@ -55,21 +55,27 @@ for file in range(filenum):
                     verty = vertexData[1]
                     vertz = vertexData[2]
                     x0 = vertx
-                    x0 = vertx
-                    y0 = verty*math.cos(math.radians(45)) + vertz*math.sin(math.radians(45))
-                    z0 = vertz*math.cos(math.radians(45)) - verty*math.sin(math.radians(45))
+                    y0 = verty*math.cos(math.radians(0)) + vertz*math.sin(math.radians(0))
+                    z0 = vertz*math.cos(math.radians(0)) - verty*math.sin(math.radians(0))
                     x1 = x0*math.cos(math.radians(45)) - z0*math.sin(math.radians(45))
                     y1 = y0
                     z1 = z0*math.cos(math.radians(45)) + x0*math.sin(math.radians(45))
                     x2 = x1*math.cos(math.radians(0)) + y1*math.sin(math.radians(0))
                     y2 = y1*math.cos(math.radians(0)) - x1*math.sin(math.radians(0))
+                    y2 = y2 * -1
                     vertices.append([x2,y2])
-                point1 = vertices[0][0] * 40
-                point2 = vertices[0][1] * 40
-                point3 = vertices[1][0] * 40
-                point4 = vertices[1][1] * 40
-                point5 = vertices[2][0] * 40
-                point6 = vertices[2][1] * 40
+                point1 = vertices[0][0] * 8
+                point2 = vertices[0][1] * 8
+                point3 = vertices[1][0] * 8
+                point4 = vertices[1][1] * 8
+                point5 = vertices[2][0] * 8
+                point6 = vertices[2][1] * 8
+                point1 += 250
+                point2 += 250
+                point3 += 250
+                point4 += 250
+                point5 += 250
+                point6 += 250
                 string = str(point1) + (',') + str(point2) + (' ') + str(point3) + (',') + str(point4) + (' ') + str(point5) + (',') + str(point6)
                 et.SubElement(doc, 'polyline', points = string, stroke='lightblue', fill='blue')
                 
